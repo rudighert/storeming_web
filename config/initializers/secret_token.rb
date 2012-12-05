99643ceef370cd51c070ff7252813fd4f56036a0
Rudy iMac <rudighert@gmail.com>
2012-12-04 21:03:12 -0300

    creación del repositorio del sitio

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StoremingWeb::Application.config.secret_token = '83a338b663df6f5100e9a6a1acbded137ec3ba72ea86e433cd17f53b66626021d70c36131479545f9e3bd776ad2d3c5ced53403931b3e3bdcf9e8bbead0ef7d7'
